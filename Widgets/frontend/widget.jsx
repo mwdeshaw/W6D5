b847aa0f4ad45ca1b7d7316584e2c4cee058f874
Matt DeShaw <mwdeshaw@gmail.com>
2019-06-21 16:57:12 -0700

    Improve tabs 2 and 3

--- Widgets/frontend/widget.jsx
+++ Widgets/frontend/widget.jsx
@@ -14,8 +14,12 @@ class Widget extends React.Component {
         <Clock/>
         <Tabs tabs={[
           {title: 'Tab 1', content: 'Tab 1 Content'},
-          {title: 'Tab 2', content: 'Tab 2 Content'},
-          {title: 'Tab 3', content: 'Tab 3 Content'},
+          {title: 'Tab 2', content: <img src='../dist/Ronil_Bhatia.jpg'/>},
+          { title: 'Tab 3', content: 
+            <div class='stache'>
+              <img src='../dist/Ronil_Bhatia.jpg' />
+              <i class='mustache-solid icon'></i>
+            </div>},
         ]}/>
       </div>
     );
